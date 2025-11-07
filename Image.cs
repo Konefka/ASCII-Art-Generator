df940fde106c0e89e381f445939fde74e2d61320
Konefka <tymhok@konto.pl>
2025-11-07 17:20:48 +0100

    Dodaj pliki projektów.

---
+++ Image.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Text;
+
+namespace ASCII_Art_Generator
+{
+    internal sealed class Generator
+    {
+        public string GenerateAsciiArtFromImage(Stream inputStream)
+        {
+            var asciiChars = "@%#*+=-:,. ";
+            // var asciiChars = "\"$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. \"";
+
+            using var image = new Bitmap(inputStream);
+
+            int outputWidth = image.Width / 3;
+            int widthStep = Math.Max(1, image.Width / outputWidth);
+            int outputHeight = image.Height / 3;
+            int heightStep = Math.Max(1, image.Height / outputHeight);
+
+            StringBuilder asciiBuilder = new(outputWidth * outputHeight);
+            for (var h = 0; h < image.Height; h += heightStep)
+            {
+                for (var w = 0; w < image.Width; w += widthStep)
+                {
+                    var pixelColor = image.GetPixel(w, h);
+                    int grayValue = (int)(pixelColor.R * 0.3 + pixelColor.G * 0.59 + pixelColor.B * 0.11);
+                    var asciiChar = asciiChars[grayValue * (asciiChars.Length - 1) / 255];
+                    asciiBuilder.Append(asciiChar);
+                    asciiBuilder.Append(asciiChar);
+                }
+                asciiBuilder.AppendLine();
+            }
+
+            return asciiBuilder.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using ASCII_Art_Generator;
+
+string imagePath = @"C:\Users\Tymek\Desktop\screenshots\SlimeRancher-2021-09-19-10-59-35-39.png";
+
+using var inputStream = new FileStream(imagePath, FileMode.Open, FileAccess.Read);
+var generator = new Generator();
+
+string asciiArt = generator.GenerateAsciiArtFromImage(inputStream);
+Console.WriteLine(asciiArt);
